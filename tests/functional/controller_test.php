cf99715f9a9539b3aa9653184f516225de6fa616
rxu <rxu@mail.ru>
2018-01-06 19:37:38 +0700

    Add controller tests.

---
+++ tests/functional/controller_test.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * Thanks for posts extension. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2018, rxu, www.phpbbguru.net
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace gfksx\ThanksForPosts\tests\functional;
+
+/**
+ * @group functional
+ */
+class controller_test extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('gfksx/ThanksForPosts');
+	}
+
+	public function test_thanklist()
+	{
+		$this->create_user('user1');
+		$this->add_user_group('ADMINISTRATORS', array('user1'));
+		$this->login('user1');
+
+		// Create a post
+		$topic = $this->create_topic(2, 'Test Topic 1', 'This is a first test topic posted by the testing framework.');
+		$post = $this->create_post(2, $topic['topic_id'], 'Re: Test Topic 1', 'This is a reply to the first test topic posted by the testing framework.');
+
+		// Logout user1
+		$this->logout();
+		// Login as admin
+		$this->login();
+
+		// Create a thank for the post
+		$crawler = self::request('GET', "viewtopic.php?f=2&t={$post['topic_id']}&sid={$this->sid}&p={$post['post_id']}#p{$post['post_id']}");
+		$thanks_link = str_replace('./', '', html_entity_decode($crawler->filter('#lnk_thanks_post' . $post['post_id'])->attr('href')));
+		$crawler = self::request('GET', $thanks_link);
+
+		$this->add_lang_ext('gfksx/ThanksForPosts', 'thanks_mod');
+
+		$crawler = self::request('GET', 'app.php/thankslist');
+		$this->assertContains($this->lang('THANKS_USER'), $crawler->filter('h2')->text());
+		$this->assertContains('2 users', $crawler->filter('div[class="pagination"]')->text());
+		$this->assertContains('user1', $crawler->filter('a[class="username"]')->text());
+		$this->assertContains('admin', $crawler->filter('a[class="username-coloured"]')->text());
+	}
+
+	public function test_toplist()
+	{
+		$this->add_lang_ext('gfksx/ThanksForPosts', 'thanks_mod');
+
+		$this->login();
+
+		$this->admin_login();
+		$crawler = self::request('GET', "adm/index.php?sid={$this->sid}&i=-gfksx-ThanksForPosts-acp-acp_thanks_reput_module&mode=thanks");
+		$form = $crawler->selectButton('Submit')->form();
+		$values = $form->getValues();
+		// Enable forum rating rating options (post and topic ones are enabled by default)
+		$values['config[thanks_forum_reput_view]'] = true;
+		$form->setValues($values);
+		$crawler = self::submit($form);
+		$this->assertContains($this->lang('CONFIG_UPDATED'), $crawler->filter('.successbox')->text());
+
+		$crawler = self::request('GET', 'app.php/toplist');
+		$this->assertContains($this->lang('TOPLIST'), $crawler->filter('h2')->text());
+		$this->assertContains($this->lang('RATING_TOP_POST'), $crawler->filter('h3')->eq(0)->text());
+		$this->assertContains($this->lang('RATING_TOP_TOPIC'), $crawler->filter('h3')->eq(1)->text());
+		$this->assertContains($this->lang('RATING_TOP_FORUM'), $crawler->filter('h3')->eq(2)->text());
+
+		$crawler = self::request('GET', "adm/index.php?sid={$this->sid}&i=-gfksx-ThanksForPosts-acp-acp_thanks_reput_module&mode=thanks");
+		$form = $crawler->selectButton('Submit')->form();
+		$values = $form->getValues();
+		// Disable all rating options
+		$values['config[thanks_post_reput_view]'] = false;
+		$values['config[thanks_topic_reput_view]'] = false;
+		$values['config[thanks_forum_reput_view]'] = false;
+		$form->setValues($values);
+		$crawler = self::submit($form);
+		$this->assertContains($this->lang('CONFIG_UPDATED'), $crawler->filter('.successbox')->text());
+
+		$crawler = self::request('GET', 'app.php/toplist');
+		$this->assertContains($this->lang('RATING_VIEW_TOPLIST_NO'), $crawler->filter('html')->text());
+		$this->assertNotContains($this->lang('TOPLIST'), $crawler->filter('ul[class="dropdown-contents"]')->text());
+		$this->assertCount(0, $crawler->filter('.fa-star-o'));
+	}
+}
--- tests/functional/thanking_test.php
+++ tests/functional/thanking_test.php
@@ -15,66 +15,36 @@ namespace gfksx\ThanksForPosts\tests\functional;
  */
 class thanking_test extends \phpbb_functional_test_case
 {
-	static protected function setup_extensions()
-	{
-		return array('gfksx/ThanksForPosts');
-	}
-
 	public function test_thanking()
 	{
-		$user1_id = $this->create_user('user1');
+		// user1 account, its post and a thank should have been created in controller_test.php
 
-		$this->add_user_group('ADMINISTRATORS', array('user1'));
-
-		$this->login('user1');
-
-		// Create a topic
-		$topic = $this->create_topic(2, 'Test Topic 1', 'This is a first test topic posted by the testing framework.');
-		$post = $this->create_post(2, $topic['topic_id'], 'Re: Test Topic 1', 'This is a reply to the first test topic posted by the testing framework.');
-
-		// Logout user1 and login as admin
-		$this->logout();
 		$this->login();
+
+		$this->get_db;
+		$sql = 'SELECT post_id, topic_id FROM ' . POSTS_TABLE . '
+			ORDER BY post_id DESC LIMIT 1';
+		$result = $this->db->sql_query($sql);
+		$post = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
 
 		// Now test thanks button exist
 		$crawler = self::request('GET', "viewtopic.php?f=2&t={$post['topic_id']}&sid={$this->sid}&p={$post['post_id']}#p{$post['post_id']}");
-
 		$thanks_button_title = $crawler->filter('a[id="lnk_thanks_post' . $post['post_id'] . '"]')->attr('title');
-		$thanks_link = html_entity_decode($crawler->filter('#lnk_thanks_post' . $post['post_id'])->attr('href'));
-		$this->assertEquals('Say Thanks to the author of the post: user1', $thanks_button_title);
+		$thanks_link = str_replace('./', '', html_entity_decode($crawler->filter('a[id="lnk_thanks_post' . $post['post_id'] . '"]')->attr('href')));
+		$this->assertContains('These users thanked the author', $crawler->filter('div[id="list_thanks' . $post['post_id'] . '"]')->text());
+		$this->assertEquals('Remove your thanks: user1', $thanks_button_title);
 
 		// Test thanking process
 		$crawler = self::request('GET', $thanks_link);
-		sleep(10);
-		$crawler = self::request('GET', "viewtopic.php?f=2&t={$post['topic_id']}&sid={$this->sid}&p={$post['post_id']}#p{$post['post_id']}");
-		$this->assertContains('These users thanked the author', $crawler->filter('html')->text());
+		$this->assertContains('Remove your thanks:', $crawler->filter('h2')->text());
 
-		$thanks_button_title = $crawler->filter('#lnk_thanks_post' . $post['post_id'])->attr('title');
-		$this->assertEquals('Remove your thanks: user1', $thanks_button_title);
-
-
-/*		$form = $crawler->selectButton('Submit')->form();
-		$form->setValues(array('message' => 'This is a post which SHOULD BE merged with the previous one.'));
+		$form = $crawler->selectButton('Yes')->form();
 		$crawler = self::submit($form);
 
-		$this->assertContains('Added in', $crawler->filter('html')->text());*/
+		$crawler = self::request('GET', "viewtopic.php?f=2&t={$post['topic_id']}&sid={$this->sid}&p={$post['post_id']}#p{$post['post_id']}");
+
+		$thanks_button_title = $crawler->filter('#lnk_thanks_post' . $post['post_id'])->attr('title');
+		$this->assertEquals('Say Thanks to the author of the post: user1', $thanks_button_title);
 	}
-/*
-	public function test_ignore_merging_posts()
-	{
-		$this->login();
-
-		// Create a topic
-		$post = $this->create_topic(2, 'Test Topic 2', 'This is a second test topic posted by the testing framework.');
-
-		// Test the ignore option checkbox is present
-		$crawler = self::request('GET', "posting.php?mode=reply&f=2&t={$post['topic_id']}&sid={$this->sid}");
-		$this->assertContains('Do not merge with previous post', $crawler->filter('html')->text());
-
-		// Test option to ignore merging posts
-		$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 2', 'This is a post which should NOT be merged with the previous one.', array('posts_merging_option' => true));
-
-		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
-		$this->assertNotContains('Added in', $crawler->filter('html')->text());
-	}*/
 }
