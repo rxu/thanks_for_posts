73f091c2039b781692d5cd92fc6be275366b342d
rxu <rxu@mail.ru>
2018-02-24 13:21:30 +0700

    Bump version to 2.0.5.

---
+++ migrations/v_2_0_5.php
@@ -0,0 +1,46 @@
+<?php
+/**
+*
+* Thanks For Posts extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace gfksx\thanksforposts\migrations;
+
+class v_2_0_5 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['thanks_for_posts_version']) && version_compare($this->config['thanks_for_posts_version'], '2.0.5', '>=');
+	}
+
+	static public function depends_on()
+	{
+			return array(
+				'\gfksx\thanksforposts\migrations\v_2_0_4',
+			);
+	}
+
+	public function update_schema()
+	{
+		return array(
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Current version
+			array('config.update', array('thanks_for_posts_version', '2.0.5')),
+		);
+	}
+}
