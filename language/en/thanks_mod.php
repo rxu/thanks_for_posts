cd76971b5bfebf3e80ffdbcc2e98b244770aeb2c
SiavaRu <siava@siava.ru>
2018-11-09 15:04:47 +0300

    Update thanks_mod.php

--- language/en/thanks_mod.php
+++ language/en/thanks_mod.php
@@ -64,8 +64,8 @@ $lang = array_merge($lang, array(
 	'NO_VIEW_USERS_THANKS'		=> 'You are not authorised to view the Thanks List.',
 
 	'NOTIFICATION_THANKS_GIVE'	=> array(
-		1 => '<strong>Received thank</strong> from %1$s for the post:',
-		2 => '<strong>Received thanks</strong> from %1$s for the post:',
+		1 => '%1$s <strong>has thanked</strong> you for this post:',
+		2 => '%1$s <strong>has thanked</strong> you for this post:',
 	),
 	'NOTIFICATION_THANKS_REMOVE'=> array(
 		1 => '<strong>Removed thank</strong> from %1$s for the post:',
